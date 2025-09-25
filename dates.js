cf5e5f4948b6ad63dded7d5260205ebd22d5bdaf
shambhavichaurasia <shamchaurasia1504@gmail.com>
2025-09-25 21:35:05 +0530

    dates

---
+++ dates.js
@@ -0,0 +1,9 @@
+let mydate=new Date();
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toTimeString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toISOString());
+console.log(mydate.toUTCString());
